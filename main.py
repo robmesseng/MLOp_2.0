8cc8e42146dfd8c72f25f9c523d2841cee0f2218
robmesseng <robmessenger.ing@gmail.com>
2024-04-20 01:51:21 -0400

    Entrenamiento con yaml y checkpoint guardado final.ckpt

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 from src import *
 from pathlib import Path
 import pytorch_lightning as pl
+import yaml
+import sys
 
-path = Path('dataset')
-dm = MNISTDataModule(path, batch_size=25)
-module = MNISTModule()
-trainer = pl.Trainer(
-    max_epochs=10,
-    logger=None,
-    enable_checkpointing=False,
-    # overfit_batches=1,
-)
-trainer.fit(module, dm)
+
+config = {
+    'datamodule': {
+        'path': Path('dataset'),
+        'batch_size': 25
+    },
+    'trainer': {
+        'max_epochs': 10,
+        'logger': None,
+        'enable_checkpointing': False,
+        'overfit_batches': 0
+    }
+}
+
+
+def train(config):
+    dm = MNISTDataModule(**config['datamodule'])
+    module = MNISTModule()
+    trainer = pl.Trainer(**config['trainer'])
+    trainer.fit(module, dm)
+    trainer.save_checkpoint('final.ckpt')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+        if config_file:
+            with open(config_file, 'r') as stream:
+                loaded_config = yaml.safe_load(stream)
+            deep_update(config, loaded_config)
+    print(config)
+    train(config)
